05e1d9b89532736790ee350380bfe1b49834d759
darkstarline <darkstarline@outlook.com>
2021-04-10 14:27:45 +0800

    @darkstarline@前台页面基本框架先搭起来了，准备完善后台功能。

---
+++ src/main/webapp/views/checkin/checkout.js
@@ -0,0 +1,78 @@
+define(function(require, exports, module) {
+    console.log("load js");
+    // API接口注册工具
+    var requireAPI = require('utils/api');
+    var API = requireAPI.init();
+    // Bootstrap-Table 常用方法工具
+    // var BTable = require('utils/btable');
+    // Ajax
+    var Ajax = require('utils/ajax');
+    // Handlebars
+    // var HBS = require('utils/hbs');
+    //Bootstrap-Dialog 常用方法工具
+    // var BDialog = require('utils/bdialog');
+    // jQuery-Form 常用方法工具
+    var jqForm = require('utils/jqform');
+    // 引入jQuery插件，用于信提示等;
+    require('utils/jqPlugin');
+    // 查询
+    // API.add("fileList", "travel.json", "");
+    // API.add("catalogList", "fileList.json", "upass/fileBrowse/getCatalogs");
+    // API.add("fileList", "fileList.json", "upass/fileBrowse/getFiles");
+    // API.add("viewerPic", "../assets/images/1900x1200_img_7.png", "upass/download/downloadNoToken");
+    // 获取信息
+    // API.add("getUserInfo", "userInfo.json", "userInfo");
+    // 定义当前页面对象
+    var Page = {
+        init: function() {
+            this._render();
+        },
+        _render: function() {
+            Page.operate.init();
+        }
+    }
+    Page.data = {
+
+    }
+
+    Page.action = {
+        // 查询所有列表接口
+        queryList: function() {
+            Ajax.postJson(API.get('fileList'), null, function (json, state) {
+                var organismList=eval('json.'+"organismList");
+                for(var i=0;i<organismList.length;i++){
+                    Utils.justiceWatching(organismList[i]);
+                }
+            })
+        },
+    };
+
+    // 定义页面所有的操作方法
+    Page.operate = {
+        init: function() {
+            this.tt();
+        },
+        // 路径点击跳转事件
+        tt: function() {
+            $(".btn").on('click', function(event) {
+                var cmd = Page.action.getParams($form);
+                cmd = this.jsonToStr(cmd);
+                Ajax.postJson(API.get(''), cmd, function (json, state) {
+                    if (state) {
+                        $.message('修改成功!');
+                        $addAuth.close(); // 关闭修改弹框
+                        $table.refresh();//刷新表格
+                    } else {
+                        $.message({message: json.message, type: 'warning'});
+                    }
+                })
+            });
+        },
+    }
+    var Utils = {
+        justiceWatching: function (organism){
+            
+        }
+    }
+    module.exports = Page;
+});
